4d5107070140a48f4257c6922b612c3f4de49872
Shashank Singh <Shashank Singh>
2023-09-08 21:29:57 +0530

    Add basic HTML page and added JS logic

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-const rock = document.getElementById("rock");
-const paper = document.getElementById("paper");
-const scissor = document.getElementById("scissor");
-
+let userScore = document.querySelector(".player-score");
+let compScore = document.querySelector(".computer-score");
+let rock = document.getElementById("rock");
+let paper = document.getElementById("paper");
+let scissor = document.getElementById("scissor");
+let result = document.querySelector(".result");
 // Get computer's choice
 const getComputerChoice = function () {
   const choices = ["rock", "paper", "scissors"];
@@ -15,44 +17,44 @@ function playRound(playerSelection, computerSelection) {
   //   console.log(computerSelection);
   if (playerSelection.toLowerCase() == "rock" && computerSelection == "paper") {
     computerScore += 1;
-    return "You Lose! Paper beats Rock";
+    result.textContent = "You Lose! Paper beats Rock";
   } else if (
     playerSelection.toLowerCase() == "rock" &&
     computerSelection == "scissors"
   ) {
     playerScore += 1;
-    return "You Win! Rock beats Scissors";
+    result.textContent = "You Win! Rock beats Scissors";
   } else if (
     playerSelection.toLowerCase() == "paper" &&
     computerSelection == "rock"
   ) {
     playerScore += 1;
-    return "You Win! Paper beats Rock";
+    result.textContent = "You Win! Paper beats Rock";
   } else if (
     playerSelection.toLowerCase() == "paper" &&
     computerSelection == "scissors"
   ) {
     computerScore += 1;
-    return "You Lose! Scissors beats Paper";
+    result.textContent = "You Lose! Scissors beats Paper";
   } else if (
     playerSelection.toLowerCase() == "scissors" &&
     computerSelection == "paper"
   ) {
     playerScore += 1;
-    return "You Win! Scissors beats Paper";
+    result.textContent = "You Win! Scissors beats Paper";
   } else if (
     playerSelection.toLowerCase() == "scissors" &&
     computerSelection == "rock"
   ) {
     computerScore += 1;
-    return "You Lose! Rock beats Scissors";
+    result.textContent = "You Lose! Rock beats Scissors";
   } else {
-    return "It's a draw";
+    result.textContent = "It's a draw";
   }
 }
 
 // console.log(playRound(playerSelection, getComputerChoice()));
-
+/*
 const game = function () {
   let playerSelection;
   for (let i = 0; i < 5; i++) {
@@ -67,5 +69,20 @@ const game = function () {
     console.log(`Computer won! ${computerScore} vs ${playerScore}`);
   else console.log("It's a draw");
 };
+*/
 
-game();
+const game = function (selection) {
+  playRound(selection, getComputerChoice());
+  userScore.textContent = playerScore;
+  compScore.textContent = computerScore;
+};
+
+rock.addEventListener("click", function () {
+  game("rock");
+});
+paper.addEventListener("click", function () {
+  game("paper");
+});
+scissor.addEventListener("click", function () {
+  game("scissors");
+});
