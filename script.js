6b20bbef1fdc6d8e48ce8961ca30ba1ce48d4318
Shashank Singh <Shashank Singh>
2023-09-08 23:26:34 +0530

    Completed the logic, Added Play Again Button

--- script.js
+++ script.js
@@ -3,53 +3,56 @@ let compScore = document.querySelector(".computer-score");
 let rock = document.getElementById("rock");
 let paper = document.getElementById("paper");
 let scissor = document.getElementById("scissor");
-let result = document.querySelector(".result");
-// Get computer's choice
+let roundResult = document.querySelector(".round-result");
+let gameResult = document.querySelector(".game-result");
+let resetButton = document.getElementById("reset");
+let rounds = 0;
+let playerScore = 0;
+let computerScore = 0;
+
 const getComputerChoice = function () {
   const choices = ["rock", "paper", "scissors"];
   return choices[Math.floor(Math.random() * 3)];
 };
-let playerScore = 0;
-let computerScore = 0;
-// Get the user's choice
+
 // Compare User's choice with computer's choice to get the winner
 function playRound(playerSelection, computerSelection) {
   //   console.log(computerSelection);
   if (playerSelection.toLowerCase() == "rock" && computerSelection == "paper") {
     computerScore += 1;
-    result.textContent = "You Lose! Paper beats Rock";
+    roundResult.textContent = "You Lose! Paper beats Rock";
   } else if (
     playerSelection.toLowerCase() == "rock" &&
     computerSelection == "scissors"
   ) {
     playerScore += 1;
-    result.textContent = "You Win! Rock beats Scissors";
+    roundResult.textContent = "You Win! Rock beats Scissors";
   } else if (
     playerSelection.toLowerCase() == "paper" &&
     computerSelection == "rock"
   ) {
     playerScore += 1;
-    result.textContent = "You Win! Paper beats Rock";
+    roundResult.textContent = "You Win! Paper beats Rock";
   } else if (
     playerSelection.toLowerCase() == "paper" &&
     computerSelection == "scissors"
   ) {
     computerScore += 1;
-    result.textContent = "You Lose! Scissors beats Paper";
+    roundResult.textContent = "You Lose! Scissors beats Paper";
   } else if (
     playerSelection.toLowerCase() == "scissors" &&
     computerSelection == "paper"
   ) {
     playerScore += 1;
-    result.textContent = "You Win! Scissors beats Paper";
+    roundResult.textContent = "You Win! Scissors beats Paper";
   } else if (
     playerSelection.toLowerCase() == "scissors" &&
     computerSelection == "rock"
   ) {
     computerScore += 1;
-    result.textContent = "You Lose! Rock beats Scissors";
+    roundResult.textContent = "You Lose! Rock beats Scissors";
   } else {
-    result.textContent = "It's a draw";
+    roundResult.textContent = "It's a draw";
   }
 }
 
@@ -77,12 +80,54 @@ const game = function (selection) {
   compScore.textContent = computerScore;
 };
 
+const reset = function () {
+  playerScore = 0;
+  computerScore = 0;
+  rounds = 0;
+  userScore.textContent = 0;
+  compScore.textContent = 0;
+  gameResult.textContent = "";
+  roundResult.textContent = "";
+  userScore.textContent = 0;
+  compScore.textContent = 0;
+  rock.removeAttribute("disabled");
+  paper.removeAttribute("disabled");
+  scissor.removeAttribute("disabled");
+  resetButton.style.visibility = "hidden";
+};
+
+const checkWinner = function (playerScore, computerScore) {
+  console.log(playerScore);
+  if (rounds === 5) {
+    if (playerScore > computerScore)
+      gameResult.textContent = `You won! ${playerScore} vs ${computerScore}`;
+    else if (computerScore > playerScore)
+      gameResult.textContent = `Computer won! ${computerScore} vs ${playerScore}`;
+    else gameResult.textContent = "It's a draw";
+    resetButton.style.visibility = "visible";
+    console.log(rock);
+    rock.setAttribute("disabled", "disabled");
+    paper.setAttribute("disabled", "disabled");
+    scissor.setAttribute("disabled", "disabled");
+  }
+};
+
 rock.addEventListener("click", function () {
+  rounds += 1;
   game("rock");
+  checkWinner(playerScore, computerScore);
 });
 paper.addEventListener("click", function () {
+  rounds += 1;
   game("paper");
+  checkWinner(playerScore, computerScore);
 });
 scissor.addEventListener("click", function () {
+  rounds += 1;
   game("scissors");
+  checkWinner(playerScore, computerScore);
+});
+
+resetButton.addEventListener("click", function () {
+  reset();
 });
